956c902dffb65bd92f7d2a913270f6aec316c95b
Alvaro Galvan <alvaro@alvarogalvan.com>
2025-02-01 08:37:39 -0600

    App - Added code for CORS Middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from typing import Annotated
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://127.0.0.1:8000/", "http://localhost", "https://localhost"]
+app.add_middleware(
+    CORSMiddleware(
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials=True,
+    )
+)
 
 
 @app.get("/")
