07c9a6bda8ad1f85c5cf90590d3908e2bd06f704
Alvaro Galvan <alvaro@alvarogalvan.com>
2025-02-21 11:51:32 -0600

    DB new refactor - setp 1

--- app/db/db.py
+++ app/db/db.py
@@ -1,229 +1,238 @@
-from typing import TypeVar, Type
+from sqlmodel import SQLModel, create_engine, Session
 import logging
 
-from sqlmodel import Session, create_engine, SQLModel, select
-
-from app.db.db_models import *
+# from app.db.db_models import *
+# from typing import TypeVar, Type
 
 
 class DB:
-    # Initiate database
     def __init__(self, filepath: str) -> None:
-        # Create DB Engine
+        """Initialize the database engine and create tables."""
         self.engine = create_engine(
             f"sqlite:///{filepath}", connect_args={"check_same_thread": False}
         )
-        # Create tables referenced by foreign keys first
-        SQLModel.metadata.create_all(
-            self.engine,
-            tables=[
-                User.__table__,
-                CharacterProfile.__table__,
-                Assistant.__table__,
-                CharacterData.__table__,
-            ],
-        )
-        # Create tables that reference them
-        SQLModel.metadata.create_all(
-            self.engine,
-            tables=[Thread.__table__, Message.__table__, UserCharacter.__table__],
-        )
+        self.create_tables()
 
-        # Load admin user
-        self.initialize_admin_user()
+    def create_tables(self) -> None:
+        """Create all tables if they don't exist."""
+        SQLModel.metadata.create_all(self.engine)
 
-    def initialize_admin_user(self) -> None:
-        with Session(self.engine) as session:
-            try:
-                self.read_from_db(session, User, "user_name", "Admin")
-                logging.info("Admin loaded in database")
-            except LookupError:
-                logging.warning("No admin found. Creating new admin user...")
-                admin_user = User(user_name="Admin", email="admin@system.local")
+    def get_session(self) -> Session:
+        """Return a new session instance."""
+        return Session(self.engine)
 
-                try:
-                    self.store_entry(session, admin_user)
-                    logging.info("Admin user recorded succesfully.")
-                except Exception as e:
-                    logging.error(f"Failed to record admin user: {e}")
-                    session.rollback()
 
-    # Define generic type
-    T = TypeVar("T", bound=SQLModel)
+#         # Create tables referenced by foreign keys first
+#         SQLModel.metadata.create_all(
+#             self.engine,
+#             tables=[
+#                 User.__table__,
+#                 CharacterProfile.__table__,
+#                 Assistant.__table__,
+#                 CharacterData.__table__,
+#             ],
+#         )
+#         # Create tables that reference them
+#         SQLModel.metadata.create_all(
+#             self.engine,
+#             tables=[Thread.__table__, Message.__table__, UserCharacter.__table__],
+#         )
 
-    # CREATE #
+#         # Load admin user
+#         self.initialize_admin_user()
 
-    # Generic storage
-    def store_entry(self, session: Session, data: T) -> None:
-        """Store a new entry of any kind to the database."""
-        try:
-            logging.info(f"Storing {data.__class__.__name__}:{data}")
-            session.add(data)
-            session.commit()
-            session.refresh(data)
-            logging.info(f"{data.__class__.__name__} stored succesfully.")
-        except Exception as e:
-            session.rollback()
-            logging.error(f"Error storing {data.__class__.__name__}: {str(e)}")
-            raise
+#     def initialize_admin_user(self) -> None:
+#         with Session(self.engine) as session:
+#             try:
+#                 self.read_from_db(session, User, "user_name", "Admin")
+#                 logging.info("Admin loaded in database")
+#             except LookupError:
+#                 logging.warning("No admin found. Creating new admin user...")
+#                 admin_user = User(user_name="Admin", email="admin@system.local")
 
-    # READ #
+#                 try:
+#                     self.store_entry(session, admin_user)
+#                     logging.info("Admin user recorded succesfully.")
+#                 except Exception as e:
+#                     logging.error(f"Failed to record admin user: {e}")
+#                     session.rollback()
 
-    # Generic read
-    def read_from_db(
-        self, session: Session, table: Type[T], field: str, value: str | int
-    ) -> T:
-        """Fetch a single record from the database based on a specific field and value."""
+#     # Define generic type
+#     T = TypeVar("T", bound=SQLModel)
 
-        if table.__tablename__ not in SQLModel.metadata.tables:
-            raise ValueError(f"Table {table.__tablename__} does not exist")
+#     # CREATE #
 
-        if not hasattr(table, field):
-            raise ValueError(f"Field {field} does not exist in {table.__tablename__}")
+#     # Generic storage
+#     def store_entry(self, session: Session, data: T) -> None:
+#         """Store a new entry of any kind to the database."""
+#         try:
+#             logging.info(f"Storing {data.__class__.__name__}:{data}")
+#             session.add(data)
+#             session.commit()
+#             session.refresh(data)
+#             logging.info(f"{data.__class__.__name__} stored succesfully.")
+#         except Exception as e:
+#             session.rollback()
+#             logging.error(f"Error storing {data.__class__.__name__}: {str(e)}")
+#             raise
 
-        logging.info(f"Searching for value in {table.__tablename__}: {field} = {value}")
+#     # READ #
 
-        statement = select(table).where(getattr(table, field) == value)
-        result = session.exec(statement).first()
+#     # Generic read
+#     def read_from_db(
+#         self, session: Session, table: Type[T], field: str, value: str | int
+#     ) -> T:
+#         """Fetch a single record from the database based on a specific field and value."""
 
-        if not result:
-            raise LookupError(f"No record found.")
+#         if table.__tablename__ not in SQLModel.metadata.tables:
+#             raise ValueError(f"Table {table.__tablename__} does not exist")
 
-        logging.info("Record found.")
-        return result
+#         if not hasattr(table, field):
+#             raise ValueError(f"Field {field} does not exist in {table.__tablename__}")
 
-    # Read all
-    def read_all(
-        self, table: Type[T], session: Session, field: str | None = None
-    ) -> list[T]:
-        """Return all values from a table if no column is provided, or all the values from a specific column if it is."""
+#         logging.info(f"Searching for value in {table.__tablename__}: {field} = {value}")
 
-        if table.__tablename__ not in SQLModel.metadata.tables:
-            raise ValueError(f"Table {table.__tablename__} does not exist")
+#         statement = select(table).where(getattr(table, field) == value)
+#         result = session.exec(statement).first()
 
-        logging.info(f"Loading all records from {table.__tablename__}")
+#         if not result:
+#             raise LookupError(f"No record found.")
 
-        if field:
-            if not hasattr(table, field):
-                raise ValueError(f"Field does not exist in {table.__tablename__}")
-            statement = select(getattr(table, field))
-        else:
-            statement = select(table)
+#         logging.info("Record found.")
+#         return result
 
-        results = session.exec(statement).all()
+#     # Read all
+#     def read_all(
+#         self, table: Type[T], session: Session, field: str | None = None
+#     ) -> list[T]:
+#         """Return all values from a table if no column is provided, or all the values from a specific column if it is."""
 
-        if not results:
-            raise LookupError(f"No records found in {table.__tablename__}")
+#         if table.__tablename__ not in SQLModel.metadata.tables:
+#             raise ValueError(f"Table {table.__tablename__} does not exist")
 
-        return results
+#         logging.info(f"Loading all records from {table.__tablename__}")
 
-    # UPDATE #
+#         if field:
+#             if not hasattr(table, field):
+#                 raise ValueError(f"Field does not exist in {table.__tablename__}")
+#             statement = select(getattr(table, field))
+#         else:
+#             statement = select(table)
 
-    # Generic update
-    def update_db(
-        self,
-        table: Type[T],
-        session: Session,
-        id_field: str,
-        id_value: str | int | float,
-        updates: dict[str, str | int | float],
-    ) -> None:
-        """Updates an existing record in the database."""
+#         results = session.exec(statement).all()
 
-        if table.__tablename__ not in SQLModel.metadata.tables:
-            raise ValueError(f"Table {table.__tablename__} does not exist")
+#         if not results:
+#             raise LookupError(f"No records found in {table.__tablename__}")
 
-        logging.info(f"Updating {table.__tablename__} with {updates}")
+#         return results
 
-        record = self.read_from_db(session, table, id_field, id_value)
+#     # UPDATE #
 
-        for field, value in updates.items():
-            if hasattr(record, field):
-                setattr(record, field, value)
-            else:
-                raise ValueError(
-                    f"Unable to update {field}: field does not exist in{table.__tablename__}"
-                )
-        try:
-            session.add(record)
-            session.commit()
-            session.refresh(record)
-            logging.info(f"{table.__tablename__} updated successfully.")
-        except Exception as e:
-            session.rollback()
-            logging.error(f"Failed to update {table.__tablename__}: {e}")
-            raise
+#     # Generic update
+#     def update_db(
+#         self,
+#         table: Type[T],
+#         session: Session,
+#         id_field: str,
+#         id_value: str | int | float,
+#         updates: dict[str, str | int | float],
+#     ) -> None:
+#         """Updates an existing record in the database."""
 
-    # DELETE #
+#         if table.__tablename__ not in SQLModel.metadata.tables:
+#             raise ValueError(f"Table {table.__tablename__} does not exist")
 
-    # Limited delete methods to avoid breaking databases's relationships
+#         logging.info(f"Updating {table.__tablename__} with {updates}")
 
-    def delete_character(self, session: Session, character_id: int) -> None:
-        """Deletes a character from the database."""
-        try:
-            character = self.read_from_db(
-                session, CharacterData, "character_id", character_id
-            )
+#         record = self.read_from_db(session, table, id_field, id_value)
 
-            session.delete(character)
-            session.commit()
-            logging.info("Character deleted.")
+#         for field, value in updates.items():
+#             if hasattr(record, field):
+#                 setattr(record, field, value)
+#             else:
+#                 raise ValueError(
+#                     f"Unable to update {field}: field does not exist in{table.__tablename__}"
+#                 )
+#         try:
+#             session.add(record)
+#             session.commit()
+#             session.refresh(record)
+#             logging.info(f"{table.__tablename__} updated successfully.")
+#         except Exception as e:
+#             session.rollback()
+#             logging.error(f"Failed to update {table.__tablename__}: {e}")
+#             raise
 
-        except LookupError:
-            raise ValueError(f"Character with ID {character_id} not found")
+#     # DELETE #
 
-        except Exception as e:
-            session.rollback()
-            logging.error(f"Failed to delete character: {e}")
-            raise
+#     # Limited delete methods to avoid breaking databases's relationships
 
-    def delete_user(self, session: Session, user_id: int) -> None:
-        """Deletes a user from the database."""
-        try:
-            user = self.read_from_db(session, User, "user_id", user_id)
+#     def delete_character(self, session: Session, character_id: int) -> None:
+#         """Deletes a character from the database."""
+#         try:
+#             character = self.read_from_db(
+#                 session, CharacterData, "character_id", character_id
+#             )
 
-            session.delete(user)
-            session.commit()
-            logging.info("User deleted.")
+#             session.delete(character)
+#             session.commit()
+#             logging.info("Character deleted.")
 
-        except LookupError:
-            raise ValueError(f"User with ID {user_id} not found")
+#         except LookupError:
+#             raise ValueError(f"Character with ID {character_id} not found")
 
-        except Exception as e:
-            session.rollback()
-            logging.error(f"Failed to delete user: {e}")
-            raise
+#         except Exception as e:
+#             session.rollback()
+#             logging.error(f"Failed to delete character: {e}")
+#             raise
 
-    # API SPECIFIC
-    def get_unmet_character(
-        self, session: Session, user_id: int
-    ) -> CharacterData | None:
-        """Returns the first character in the database the user has never seen before, or None if all have been met."""
+#     def delete_user(self, session: Session, user_id: int) -> None:
+#         """Deletes a user from the database."""
+#         try:
+#             user = self.read_from_db(session, User, "user_id", user_id)
 
-        try:
-            statement = (
-                select(CharacterData)
-                .where(
-                    CharacterData.character_id.notin_(
-                        select(UserCharacter.character_id).where(
-                            UserCharacter.user_id == user_id
-                        )
-                    )
-                )
-                .limit(1)
-            )
+#             session.delete(user)
+#             session.commit()
+#             logging.info("User deleted.")
 
-            result = session.exec(statement).first()
+#         except LookupError:
+#             raise ValueError(f"User with ID {user_id} not found")
 
-            if result:
-                logging.info(f"Found unmet character for user {user_id}.")
-                return result
-            else:
-                logging.info(f"User has met all the characters in database.")
-                return None
+#         except Exception as e:
+#             session.rollback()
+#             logging.error(f"Failed to delete user: {e}")
+#             raise
 
-        except Exception as e:
-            logging.error(
-                f"Unable to retrieve unmet characters for user {user_id}: {e}"
-            )
-            raise
+#     # API SPECIFIC
+#     def get_unmet_character(
+#         self, session: Session, user_id: int
+#     ) -> CharacterData | None:
+#         """Returns the first character in the database the user has never seen before, or None if all have been met."""
+
+#         try:
+#             statement = (
+#                 select(CharacterData)
+#                 .where(
+#                     CharacterData.character_id.notin_(
+#                         select(UserCharacter.character_id).where(
+#                             UserCharacter.user_id == user_id
+#                         )
+#                     )
+#                 )
+#                 .limit(1)
+#             )
+
+#             result = session.exec(statement).first()
+
+#             if result:
+#                 logging.info(f"Found unmet character for user {user_id}.")
+#                 return result
+#             else:
+#                 logging.info(f"User has met all the characters in database.")
+#                 return None
+
+#         except Exception as e:
+#             logging.error(
+#                 f"Unable to retrieve unmet characters for user {user_id}: {e}"
+#             )
+#             raise
