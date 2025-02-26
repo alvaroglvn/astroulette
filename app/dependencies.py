932d9bb49cab306937790ff3a038cd42e538ea65
Alvaro Galvan <alvaro@alvarogalvan.com>
2025-02-26 09:29:08 -0600

    Updated dependencies for injections

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,8 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
-from app.db.db_setup import get_db
+from app.db.db_setup import get_async_session
+from app.config import AppSettings
 
 
 # Dependency: Inject Async Database Session
 def db_dependency() -> AsyncSession:
-    return Depends(get_db)
+    return Depends(get_async_session)
+
+
+# Dependency: Inject AppSettings
+def settings_dependency() -> AppSettings:
+    return AppSettings()
