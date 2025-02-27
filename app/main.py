193ee4660900a69376be64c1c6c2969988f27fc8
Alvaro Galvan <alvaro@alvarogalvan.com>
2025-02-27 17:44:04 -0600

    First attempt to new_character route

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from typing import AsyncGenerator
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.db.db_setup import init_db
+from app.routers.route_chars import router as chars_router
 
 
 @asynccontextmanager
@@ -11,3 +12,5 @@ async def lifespan(app: FastAPI) -> AsyncGenerator:
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(chars_router, prefix="/api/v1")
--- app/routers/route_chars.py
+++ app/routers/route_chars.py
@@ -19,6 +19,7 @@ async def new_character(
         tuple[CharacterData, CharacterProfile], Depends(generate_character_dependency)
     ],
     assistant: Annotated[Assistant, Depends(generate_assistant_dependency)],
+    background_tasks: BackgroundTasks,
 ) -> Response:
     # Unpack the tuple
     character_data, character_profile = character_data_profile
@@ -31,6 +32,23 @@ async def new_character(
     character_data.assistant_id = stored_assistant.id
     character_data.profile_id = stored_profile.id
 
-    create_record(session, character_data)
+    # Store charater data
+    stored_character_data = await create_record(session, character_data)
+
+    # Create the portrait and store its url
+    if stored_character_data:
+
+        async def add_image_url() -> None:
+            if image_url := await generate_portrait(
+                settings.leonardo_api_key, stored_character_data.image_prompt
+            ):
+                await update_record(
+                    session,
+                    CharacterData,
+                    stored_character_data.id,
+                    {"image_url": image_url},
+                )
+
+    background_tasks.add_task(add_image_url)
 
     return Response(status_code=201)
